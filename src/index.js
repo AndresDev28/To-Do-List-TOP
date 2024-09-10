59547cab2c0dfac98e5108ce187670f8fc8b26f8
AndresDev28 <andresdev28@hotmail.com>
2024-09-10 06:14:45 +0200

    Creating basic styles for UI and implementing show/hide modal to create a new project

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import './assets/css/normalized.css'
 import './assets/css/style.css';
 
 
+/** Obtener el elemento del DOM para mostrar el modal de crear nuevo proyecto */
+
+const addPojectBtn = document.getElementById('addProjectBtn');
+const showProjectModal = document.getElementById('createProjectModal');
+const closeProjectModal = document.getElementById('closeBtnCreateProject');
+
+/* Evento para mostrar el modal de crear Proyecto */
+addPojectBtn.addEventListener('click', () => {
+  showProjectModal.style.display = 'block';
+  overlay.style.display = 'block';
+});
+
+/* Evento para ocultar el modal de crear Proyecto */
+
+closeProjectModal.addEventListener('click', () => {
+  showProjectModal.style.display = 'none';
+  overlay.style.display = 'none';
+})
